b58ec813d5db0c99c2f3baa3b845e30b98878d7d
Dzaky Alif Gunawan <dzakyalifgunawan19@gmail.com>
2023-03-15 09:16:19 +0700

    Update Terbaru

---
+++ latihan01/pertemuan2.php
@@ -0,0 +1,24 @@
+<?php
+    $pembeli = "Daud";
+    $uang = 15000000;
+    $laptop;
+    if($uang < 10000000){
+        $laptop = "Laptop Asus";
+    }else{
+        $laptop = "Laptop HP";
+    }
+    echo "Maka $pembeli membeli $laptop";
+    
+    //inputan
+    $mahasiswa = "Seragam Lengkap";// yang dirubah isi variabel 
+
+    //pengecekan output
+    if($mahasiswa == "Seragam Lengkap") {
+        echo "Silahkan mengikuti kuliah";
+    }else if ($mahasiswa == "Seragam Tidak Lengkap"){
+        echo "Laksanakan Hukuman";
+    }else {
+        echo "Kena SP";
+    }
+    echo "<br>";
+?>
