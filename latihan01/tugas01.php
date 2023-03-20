af751bd9be660e1fe67761aeb2b8fd17fc367ba4
Dzaky Alif Gunawan <dzakyalifgunawan19@gmail.com>
2023-03-20 19:27:58 +0700

    Update Terbaru

--- latihan01/tugas01.php
+++ latihan01/tugas01.php
@@ -5,30 +5,36 @@
         array('kode'=>'B03', 'barang'=>'penghapus', 'harga'=> 5000),
     );
     $kode = $tokoase[0]['kode'];
-    $jumlahdibeli = 2;
+    $jumlahdibeli = 3;
     $hasil;
+    
     if($kode == $tokoase[0]['kode']){
-        if($jumlahdibeli >= 3){
+        if($jumlahdibeli == 3){
             $hasil = $jumlahdibeli * (50000 - 3000);
+            echo "Dzaky membeli Buku di Toko ASE <br>";
             echo "B01 Buku ,beli sebanyak : $jumlahdibeli pcs <br>";
             echo "Jumlah diBayar : $hasil";
         }else{
             $hasil = 50000 * $jumlahdibeli;
+            echo "Dzaky membeli Buku di Toko ASE <br>";
             echo "B01 Buku ,beli sebanyak : $jumlahdibeli pcs <br>";
             echo "Jumlah diBayar : $hasil";
         }
     }elseif ($kode == $tokoase[1]['kode']){
         if($jumlahdibeli >= 6){
             $hasil = $jumlahdibeli * (10000 - 2000);
+            echo "Dzaky membeli Pulpen di Toko ASE <br>";
             echo "B02 Pulpen ,beli sebanyak : $jumlahdibeli pcs <br>";
             echo "Jumlah diBayar : $hasil";
         }else{
             $hasil = 10000 * $jumlahdibeli;
+            echo "Dzaky membeli Pulpen di Toko ASE <br>";
             echo "B02 Pulpen ,beli sebanyak : $jumlahdibeli pcs <br>";
             echo "Jumlah diBayar : $hasil";
         }
     }else {
         $hasil = 5000 * $jumlahdibeli;
+        echo "Dzaky membeli Penghapus di Toko ASE <br>";
         echo "B03 Penghapus ,beli sebanyak : $jumlahdibeli pcs <br>";
         echo "Jumlah diBayar : $hasil";
     }
@@ -40,8 +46,5 @@
             echo $a." ";
         }
         echo "<br>";
-    }
-
-    
-    
+    }  
 ?>
