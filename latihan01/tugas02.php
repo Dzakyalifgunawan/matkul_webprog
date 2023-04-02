50e02b3565a5b9d243415d5df78ec44a292d6e16
Dzaky Alif Gunawan <dzakyalifgunawan19@gmail.com>
2023-04-02 23:53:24 +0700

    Update Terbaru

--- latihan01/tugas02.php
+++ latihan01/tugas02.php
@@ -1,13 +1,40 @@
 <?php
-    $jam = date("d-m-Y, h:i:s");
+    $data = array (
+        array ('no' => '01', 'keliling' => 'baju'),
+        array ('no' => '02', 'keliling' => 'olahraga'),
+        array ('no' => '03', 'keliling' => 'pidato'),
+    );
+    $kegiatan = $data[0]['keliling'];
 
-    function kata($jam){
-        if($jam == date("s : 5")){
-            $nama = "Puan";
-            $kata = "$jam, $nama sedang membagikan baju kepada masyarakat setempat";            
-            echo "Hari ini $kata";
+    if($kegiatan == $data[0]['keliling']){
+        echo kata1();
+        foreach($data as $k){
+            echo $k."";
         }    
+    }elseif($kegiatan == $data[1]['keliling']){
+        echo kata2();
+    }else{
+        echo kata3();
     }
-    kata($jam);
-    
+
+    function kata1(){
+        $jam = date("d-m-Y, h:i:s");
+        $nama = "Puan";
+        $kata = "$jam, $nama sedang membagikan baju kepada masyarakat setempat";            
+        echo "Hari ini $kata";
+    }
+
+    function kata2(){
+        $jam = date("d-m-Y, h:i:s");
+        $nama = "Puan";
+        $kata = "$jam, $nama sedang berolahraga di gedung DPR";            
+        echo "Hari ini $kata";
+    }
+
+    function kata3(){
+        $jam = date("d-m-Y, h:i:s");
+        $nama = "Puan";
+        $kata = "$jam, $nama sedang Berpidato didepan Pak Jokowi";            
+        echo "Hari ini $kata";
+    }
 ?>
