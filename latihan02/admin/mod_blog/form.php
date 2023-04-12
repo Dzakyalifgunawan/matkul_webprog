1eeea9e31084db7321811ad339135ec2a71b2706
Dzaky Alif Gunawan <dzakyalifgunawan19@gmail.com>
2023-04-12 08:35:08 +0700

    Update Terbaru

--- latihan02/admin/mod_blog/form.php
+++ latihan02/admin/mod_blog/form.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-    <title>Form</title>
-</head>
-<body>
+
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-3"></div>
@@ -40,7 +30,7 @@
                 <div class="mb-3 row">
                     <label for="inputPassword" class="col-sm-2 col-form-label">Upload Gambar</label>
                     <div class="col-sm-7">
-                        <button type="button">choose File</button><span>No file Chosen</span>
+                        <input type="file" accept="image/*">choose File</input><span>No file Chosen</span>
                     </div>
                 </div>
                 <div class="mb-3 row">
@@ -56,6 +46,3 @@
             <div class="col-md-2"></div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-</body>
-</html>
--- latihan02/admin/mod_user/index.php
+++ latihan02/admin/mod_user/index.php
@@ -1,47 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
-    <title>Form</title>
-</head>
-<body>
-    <div class="container-fluid">
+<div class="container">
         <div class="row">
             <div class="col-md-3"></div>
-            <div class="col-md">
-                <h3>Form Input Data</h3>
-                <div class="mb-3 row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">id_user</label>
-                    <div class="col-sm-7">
-                        <input type="text" class="form-control" id="inputkategori">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Username</label>
-                    <div class="col-sm-7">
-                        <input type="text" class="form-control" id="inputkategori">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">password</label>
-                    <div class="col-sm-7">
-                        <input type="text" class="form-control" id="inputkategori">
-                    </div>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                    <label class="form-check-label" for="flexCheckChecked">is_active</label>
-                </div>
-                <button class="btn btn-secondary" type="submit"><i class="bi bi-x-lg"> Batal</i></button>
-                <button class="btn btn-primary" type="submit"><i class="bi bi-save"> Simpan</i></button>            
+            <div class="col-md mt-3">
+                <a class="btn btn-primary btn-sm" href="form.php">Tambah Data</a>
+                <table class="table table-bordered mt-1">
+                    <tr>
+                        <td scope="col">id_user</td>
+                        <td scope="col">Username</td>
+                        <td scope="col">Password</td>
+                        <td scope="col">is_Active</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <button class="btn btn-primary" type="submit"><i class="bi bi-pencil-square"> Edit</i></button>
+                            <button class="btn btn-primary" type="submit"><i class="bi bi-x-lg"> Delete</i></button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <button class="btn btn-primary" type="submit"><i class="bi bi-pencil-square"> Edit</i></button>
+                            <button class="btn btn-primary" type="submit"><i class="bi bi-x-lg"> Delete</i></button>
+                        </td>
+                    </tr>
+                </table>
             </div>
-            <div class="col-md-2"></div>
+            <div class="col-md-3"></div>
         </div>
     </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>            
-</body>
-</html>
