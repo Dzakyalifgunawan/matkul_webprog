b33b3e193dd0cff313c4e8b4c01c0494784ad51c
Dzaky Alif Gunawan <dzakyalifgunawan19@gmail.com>
2023-05-15 13:01:30 +0700

    Update Terbaru

--- latihan02/admin/mod_user/index.php
+++ latihan02/admin/mod_user/index.php
@@ -8,30 +8,28 @@ $qdata = mysqli_query($koneksidb, "select * from mst_user")or die(mysqli_error($
 ?>
 <div class="container">
         <div class="row">
-            <div class="col-md-3"></div>
+            <div class="col-md-2"></div>
             <div class="col-md mt-3">
                 <a class="btn btn-primary btn-sm" href="?modul=mod_user&aksi=add">Tambah Data</a>
-                <table class="table table-bordered mt-1">
+                <table class="table mt-1">
                     <tr>
-                        <td scope="col">id_user</td>
-                        <td scope="col">Username</td>
-                        <td scope="col">Password</td>
-                        <td scope="col">is_Active</td>
-                        <td scope="col">Action</td>
+                        <th scope="col">ID User</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Password</th>
+                        <th scope="col">Action</th>
                     </tr>
                     <?php
                         $no = 1;
                         while($row = mysqli_fetch_array($qdata)){//pembuka
                     ?>
                     <tr>
-                        <td><?php $row['id_user']; ?></td>
-                        <td><?php $row['username']; ?></td>
-                        <td><?php $row['password']; ?></td>
-                        <td></td>
+                        <td><?php echo $row['id_user']; ?></td>
+                        <td><?php echo $row['username']; ?></td>
+                        <td><?php echo $row['pass_user']; ?></td>
                         <td>
-                            <a href="?modul=mod_user&aksi=edit&id=<?php echo $row['id_user']; ?>" class="btn btn-primary" type="submit"><i class="bi bi-pencil-square"> Edit</i></a>
+                            <a href="?modul=mod_user&aksi=edit&id=<?php echo $row['id_user']; ?>" class="btn btn-primary"><i class="bi bi-pencil-square"> Edit</i></a>
                             <a href="mod_user/proses.php?proses=delete&id=<?php echo $row['id_user']; ?>" class="btn btn-primary" type="submit"><i class="bi bi-x-lg"> Delete</i></a>
-                        </td>
+                        </td>   
                     </tr>
                     <?php
                         $no++; //untuk mengganti auto increment menjadi urut 
@@ -39,7 +37,7 @@ $qdata = mysqli_query($koneksidb, "select * from mst_user")or die(mysqli_error($
                     ?>
                 </table>
             </div>
-            <div class="col-md-3"></div>
+            <div class="col-md-2"></div>
         </div>
 </div>
 <?php 
@@ -48,52 +46,58 @@ $qdata = mysqli_query($koneksidb, "select * from mst_user")or die(mysqli_error($
             $query = mysqli_query($koneksidb, "select * from mst_user where id_user=".$_GET['id']."")or die(mysqli_error($koneksidb));
             $data = mysqli_fetch_array($query);
             $nama = $data['username'];
-            $pass = $data['password'];
             $exproses = "update";
             $id = $_GET['id'];
+            $pass = $data["pass_user"];
 
         }elseif($_GET['aksi'] == "add"){
             // proses add 
             $exproses = "insert";
             $id = 0;
             $nama = "";
-        }    
+            $pass = "";
+        }
 
 ?>
+
+<!-- Tampilan Form -->
+
 <div class="container-fluid">
-    <div class="row">
+        <div class="row">
             <div class="col-md-3"></div>
-            <div class="col-md-9">
-                <h3>Form Input Data</h3>
+            <div class="col-md">
+            <h3>Form Input Data</h3>
                 <form action="mod_user/proses.php?proses=<?php echo $exproses; ?>" method="post">
                     <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">id_user</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control" id="inputkategori" name="id_user">
+                        <!-- <label for="inputPassword" class="col-sm-3 col-form-label">id_user</label> -->
+                        <div class="col-sm-9">
+                            <input type="hidden" id="id_user" name="id_user" value="<?php echo $id; ?>">
                         </div>
                     </div>
                     <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Username</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control" id="inputkategori" name="username" value="<?php echo $nama ?>">
+                        <label for="inputPassword" class="col-sm-3 col-form-label">Username</label>
+                        <div class="col-sm-9">
+                            <input type="hidden" id="txt_id" name="txt_id" value="<?php echo $id; ?>">
+                            <input type="text" class="form-control" id="txt_nama" name="txt_nama" value="<?php echo $nama ?>">
                         </div>
                     </div>
                     <div class="mb-3 row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">password</label>
-                        <div class="col-sm-7">
-                            <input type="text" class="form-control" id="inputkategori" name="password" value="">
+                        <label for="inputPassword" class="col-sm-3 col-form-label">Password</label>
+                        <div class="col-sm-9">
+                            <input type="hidden" id="txt_id" name="txt_id" value="<?php echo $id; ?>">
+                            <input type="text" class="form-control" id="txt_pass" name="txt_pass" value="<?php echo $pass ?>">
                         </div>
                     </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-                        <label class="form-check-label" for="flexCheckChecked">is_active</label>
+                    <div class="mb-3 row">
+                        <div class="col-sm-9">
+                            <button class="btn btn-secondary" type="submit"><i class="bi bi-x-lg"> Batal</i></button>
+                            <button class="btn btn-primary" type="submit"><i class="bi bi-save"> Simpan</i></button>
+                        </div>
                     </div>
-                    <button class="btn btn-secondary" type="submit"><i class="bi bi-x-lg"> Batal</i></button>
-                    <button class="btn btn-primary" type="submit"><i class="bi bi-save"> Simpan</i></button>
                 </form>            
             </div>
             <div class="col-md-2"></div>
-    </div>
+        </div>
 </div>
 <?php
     }
--- latihan02/admin/mod_user/proses.php
+++ latihan02/admin/mod_user/proses.php
@@ -2,20 +2,22 @@
 require_once("../../koneksidb.php");
 
 if($_GET['proses'] == "insert"){
-    echo "Proses Tambah"; 
-    $username = $_POST['username'];
-    $pass = $_POST['password'];
-    $exsave = mysqli_query($koneksidb, "insert into mst_user(id_user,username, password) values('', '".$username."', '".$pass."')") or die("Gagal simpan".mysqli_error($koneksidb));
+    echo "Proses Tambah";
+    $username = $_POST['txt_nama'];
+    $pass = $_POST["txt_pass"];
+    $exsave = mysqli_query($koneksidb, "insert into mst_user(username, pass_user) values('".$username."', '".$pass."')")
+    or die("Gagal simpan".mysqli_error($koneksidb));
     if($exsave){
         // ketik proses simpan berhasil
         header("Location: http://localhost/matkul_webprog/latihan02/admin/home.php?modul=mod_user");
     }
 }elseif($_GET['proses'] == "update"){
     echo "Proses Update";
-    $username = $_POST['username'];
-    $pass =$_POST['password'];
-    $iduser = $_POST['id_user'];
-    $exsave = mysqli_query($koneksidb,"UPDATE mst_user SET username = '$username' where id_user = $iduser") or die("Gagal simpan".mysqli_error($koneksidb));
+    $username = $_POST['txt_nama'];
+    $pass = $_POST['txt_pass'];
+    $id_user = $_POST["id_user"];
+    $exsave = mysqli_query($koneksidb,"UPDATE mst_user SET username = '$username', pass_user = '$pass'
+    where id_user = $id_user") or die("Gagal simpan".mysqli_error($koneksidb));
     if($exsave){
         // ketik proses simpan berhasil
         header("Location: http://localhost/matkul_webprog/latihan02/admin/home.php?modul=mod_user");
