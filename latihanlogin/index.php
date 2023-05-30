38cbf9276c8eab2c7def4fba4be7bfbd355dba13
Dzaky Alif Gunawan <dzakyalifgunawan19@gmail.com>
2023-05-31 00:12:15 +0700

    Update Terbaru

---
+++ latihanlogin/index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <div class="container">
+        <form action="#" method="post">
+            <div class="col"></div>
+            <div class="col">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Username</label>
+                    <input type="text" class="form-control" id="txt_user" name="txt_user">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label"></label>
+                    <input type="password" class="form-control" id="txt_pasw" name="txt_pasw">
+                </div>
+                    <button type="submit" class="form-control" id="exampleFormControlInput1">Login</button>
+            </div>
+            <div class="col"></div>
+        </form>
+    </div>
+</body>
+</html>
