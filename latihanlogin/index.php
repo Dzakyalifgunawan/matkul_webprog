2d12e2b253072cc84183a03fd8a92db571d9a36a
Dzaky Alif Gunawan <dzakyalifgunawan19@gmail.com>
2023-05-31 11:23:44 +0700

    tot

--- latihanlogin/index.php
+++ latihanlogin/index.php
@@ -1,29 +1,41 @@
+<?php
 
+require_once("koneksi_db.php");
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="">
+    <title>Login User</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <form action="#" method="post">
-            <div class="col"></div>
+    <div class="containerlogin">
+        <form action="checklogin.php" method="post">
+            <?php
+            session_start();
+            session_destroy();
+            if(isset($_SESSION['pesan'])){
+                echo $_SESSION['pesan'];
+            }else{
+                session_unset();
+            }
+            
+            ?>
             <div class="col">
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Username</label>
-                    <input type="text" class="form-control" id="txt_user" name="txt_user">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label"></label>
-                    <input type="password" class="form-control" id="txt_pasw" name="txt_pasw">
-                </div>
-                    <button type="submit" class="form-control" id="exampleFormControlInput1">Login</button>
+                <label for="txt_user">Username</label>
+                <input type="text" id="txt_user" name="txt_user">
             </div>
-            <div class="col"></div>
+            <div class="col">
+                <label for="txt_pasw">Password</label>
+                <input type="password" id="txt_pasw" name="txt_pasw">
+            </div>
+            <div class="col">
+                <button type="submit" name="btn_login">Login</button>
+            </div>     
         </form>
     </div>
 </body>
