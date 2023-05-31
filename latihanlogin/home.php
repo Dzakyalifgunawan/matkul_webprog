f1543bab8adf4a56466b82cd99df9e53fb092389
Dzaky Alif Gunawan <dzakyalifgunawan19@gmail.com>
2023-05-31 11:21:47 +0700

    Update Terbaru

---
+++ latihanlogin/home.php
@@ -0,0 +1,20 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Selamat, <?php echo $_SESSION['namalog']; ?></h2>
+        <a href="mod_user/index.php" class="btn">Modul User</a>
+        <a href="mod_pegawai/index.php" class="btn">Modul Pegawai</a>
+    </div>
+</body>
+</html>
